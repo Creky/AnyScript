a3971916312391267dfb3b49cf6a42641970400c
HH <hhsc2004@gmail.com>
2016-01-14 14:44:42 +0800

    修复google不允许嵌入到frame中问题

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ chrome.extension.onRequest.addListener(
 	function(req, sender, sendResponse) {
 		console.log(sender.tab);
 		if (req.type == "queryRule") {
-			queryRule(req,sendResponse);
+			queryRule(req, sendResponse);
 		} else {
 			sendResponse({
 				"status": 0
@@ -11,21 +11,41 @@ chrome.extension.onRequest.addListener(
 	}
 );
 
-function queryRule(req,sendResponse){
-	var ruleDB=storedb("rulesTable");
-	var rules=new Array();
-	ruleDB.find({"domain":req.hostName},function(err,res){
-		if(!err&&res&&res.length>0){
-			for(var i=0;i<res.length;i++){
-				if(parseInt(res[i].forbid)){
+function queryRule(req, sendResponse) {
+	var ruleDB = storedb("rulesTable");
+	var rules = new Array();
+	ruleDB.find({
+		"domain": req.hostName
+	}, function(err, res) {
+		if (!err && res && res.length > 0) {
+			for (var i = 0; i < res.length; i++) {
+				if (parseInt(res[i].forbid)) {
 					continue;
 				}
-				var reg=new RegExp(res[i].url,"gi");
-				if(reg.test(req.url)){
+				var reg = new RegExp(res[i].url, "gi");
+				if (reg.test(req.url)) {
 					rules.push(res[i]);
 				}
 			}
 		}
-		sendResponse({"status":1,"rules":rules});
+		sendResponse({
+			"status": 1,
+			"rules": rules
+		});
 	});
 }
+
+chrome.webRequest.onHeadersReceived.addListener(function(details) {
+	//details.responseHeaders.splice(-2,1);
+	for (var i = 0; i < details.responseHeaders.length; i++) {
+		if (details.responseHeaders[i].name === 'x-frame-options') {
+			details.responseHeaders.splice(i, 1);
+			break;
+		}
+	}
+	return {
+		responseHeaders: details.responseHeaders
+	};
+}, {
+	urls: ["<all_urls>"]
+}, ["responseHeaders","blocking"]);
